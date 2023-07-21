ca6ff0eb4807cb9c59315ff2f9a53ef31f4eab0c
gihan79097 <gihan.p@ebipl.com>
2023-07-21 09:59:42 +0530

    complete the home page ui

--- lib/screens/main/home/home.dart
+++ lib/screens/main/home/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:grocery_app/components/custom_text.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:grocery_app/screens/main/home/widgets/product_grid.dart';
+import 'package:grocery_app/utils/assets_constants.dart';
+
+import '../../../components/custom_text.dart';
+import '../../../utils/app_colors.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,11 +16,30 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: CustomText(
-          text: "Home",
-          fontSize: 20,
+    return SafeArea(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 28),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SvgPicture.asset(AssetConstants.menuIcon),
+                SvgPicture.asset(AssetConstants.cartIcon),
+              ],
+            ),
+            const CustomText(
+              text: 'Vegetables',
+              textAlign: TextAlign.center,
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+              color: AppColors.primaryColor,
+            ),
+            const SizedBox(
+              height: 41,
+            ),
+            const ProductGrid(),
+          ],
         ),
       ),
     );
